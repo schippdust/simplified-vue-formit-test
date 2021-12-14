8c0ad184b11cdc8df94696eb7a7e4e4b154b7add
dschipperCuningham <58012058+dschipperCuningham@users.noreply.github.com>
2021-12-14 12:58:21 -0600

    added nodemon server for development testing in FormIt

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,78 +3,8 @@
     <v-row class="text-center">
       <v-col cols="12">
         <v-btn @click="buttonClickedTest(10, 10, 10)"
-          >Create Block {{ testData }}</v-btn
+          >Create Block Update Test {{ testData }}</v-btn
         >
-      </v-col>
-    </v-row>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">Welcome to Vuetify</h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank"
-            >Discord Community</a
-          >
-        </p>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
       </v-col>
     </v-row>
   </v-container>
@@ -82,6 +12,9 @@
 
 <script>
 import { mapState } from "vuex";
+let WSM = window.WSM;
+let FormIt = window.FormIt;
+
 export default {
   name: "HelloWorld",
   methods: {
@@ -90,12 +23,12 @@ export default {
       this.createBlock(w, l, h);
     },
     createBlock: async (w, l, h) => {
-      let pt1 = await window.WSM.Geom.Point3d(0, 0, 0);
-      let pt2 = await window.WSM.Geom.Point3d(w, l, h);
-      let histID = await window.FormIt.GroupEdit.GetEditingHistoryID();
+      let pt1 = await WSM.Geom.Point3d(0, 0, 0);
+      let pt2 = await WSM.Geom.Point3d(w, l, h);
+      let histID = await FormIt.GroupEdit.GetEditingHistoryID();
       console.log(histID, pt1, pt2);
 
-      let test = await window.WSM.APICreateBlock(histID, pt1, pt2);
+      let test = await WSM.APICreateBlock(histID, pt1, pt2);
       console.log(test);
     },
   },
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,14 @@
 module.exports = {
-  // transpileDependencies: ["vuetify"],
   publicPath: "",
-
+  //set this to false to get rid of the source map files
   productionSourceMap: false,
-
-  // devServer: {
-  //   proxy: "http://research.cuningham.com/",
-  // },
+  //dev server prevents CORS errors when referencing images
+  //or other web hosted assets during development
+  devServer: {
+    proxy: "https://schippdust.github.io/simplified-vue-formit-test/",
+  },
+  //index path sets the name of the file that will exist at index of the webpage
   indexPath: "plugin.html",
-
+  //dependencies written in ts will be transpiled to js before use in building application
   transpileDependencies: ["vuetify"],
 };
